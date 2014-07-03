29eca171b02141c2944f98521810c9fb5a85cbc4
Kai <Kai.Puth@student.htw-berlin.de>
2014-07-03 19:59:20 +0200

    API-Dokumentation entfernt.

--- doc/html/search/all_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['lfp_5ffile',['lfp_file',['../structlfp__file.html',1,'']]],
-  ['lfp_5fsection',['lfp_section',['../structlfp__section.html',1,'']]],
-  ['lfploader',['LfpLoader',['../class_lfp_loader.html',1,'']]]
-];
--- doc/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['load',['load',['../class_lfp_loader.html#ac2bce417c03788f509d663d31cacc3b4',1,'LfpLoader']]]
-];
